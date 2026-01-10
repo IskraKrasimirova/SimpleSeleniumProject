acf4373c97ddd67e2f88b02f00f172ef559b12b2
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2026-01-10 18:46:29 +0200

    Initial project setup

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
